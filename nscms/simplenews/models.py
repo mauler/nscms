e934db95e8ac814087289fc638b29eeca6db3333
Igor P. Leroy <ip.leroy@gmail.com>
2013-04-13 14:15:38 -0300

    Change SimpleNews class to NewsModel and remove simplenews from base namesace.

--- nscms/simplenews/models.py
+++ nscms/simplenews/models.py
@@ -1,11 +1,43 @@
 #-*- coding:utf-8 -*-
 
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
 
-from nscms.base.simplenews.models import BaseNews
+from ckeditor.fields import RichTextField
+from taggit.managers import TaggableManager
+
+from nscms.base.db.models import ContentModel
 
 
-class SimpleNews(BaseNews):
+class NewsModel(ContentModel):
+    body = RichTextField()
+    tags = TaggableManager(blank=True)
+
+    class Admin(ContentModel.Admin):
+        date_hierarchy = "publish_date"
+        search_fields = ("title", "description", "body",)
+        list_filter = ("published", )
+        list_display = (
+            "title", "admin_thumbnail", "published", "publish_date",)
+        fieldsets = (
+            (None,
+             {"fields": ("title", "tags", "description", "image", "body",)}),
+            (_(u"Publicação"),
+             {"fields": ("published", "publish_date", "expire_date",)}),
+        )
+
+    class Meta:
+        ordering = ['-publish_date']
+        verbose_name = _(u"Notícia")
+        verbose_name_plural = _(u"Notícias")
+        abstract = True
+
+    def related_by_tags(self, count=10):
+        qs = self.__class__.objects.published(order_by=None)
+        qs = qs.filter(
+            tags__pk__in=[i.pk for i in self.tags.all()]).exclude(pk=self.id)
+        return qs[:count]
 
     def get_absolute_url(self):
         return reverse(
