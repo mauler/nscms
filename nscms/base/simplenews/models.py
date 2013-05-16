232c17c988c83e4581f5ea45a260f5c250b1520d
Paulo R. Macedo Chaves <proberto.macedo@gmail.com>
2013-05-16 11:59:22 -0300

    Adds SIMPLENEWS_IMAGE_UPLOAD_TO option to settings

--- nscms/base/simplenews/models.py
+++ nscms/base/simplenews/models.py
@@ -1,13 +1,19 @@
 #-*- coding:utf-8 -*-
 
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
 from nscms.base.db.models import PublisherModel, ContentModel
 
 
+SIMPLENEWS_IMAGE_UPLOAD_TO = \
+    getattr(settings, "SIMPLENEWS_IMAGE_UPLOAD_TO", "simplenews")
+
+
 class SimpleNewsModel(ContentModel, PublisherModel):
-    image = models.ImageField(upload_to='/simplesnews/', max_length=255)
+    image = models.ImageField(
+        upload_to=SIMPLENEWS_IMAGE_UPLOAD_TO, max_length=255)
 
     class Meta:
         ordering = ['-publish_date']
