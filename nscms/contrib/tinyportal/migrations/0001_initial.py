5b485a32e323ec019b80976d40d50a645725b284
Paulo R <proberto.macedo@gmail.com>
2014-12-05 01:07:28 -0200

    Renames portal to tinyportal

--- nscms/contrib/tinyportal/migrations/0001_initial.py
+++ nscms/contrib/tinyportal/migrations/0001_initial.py
@@ -9,22 +9,22 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'Channel'
-        db.create_table(u'portal_channel', (
+        db.create_table(u'tinyportal_channel', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('slug', self.gf('django_extensions.db.fields.AutoSlugField')(allow_duplicates=False, max_length=255, separator=u'-', blank=True, populate_from='title', overwrite=True)),
             ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, db_index=True, blank=True)),
             ('modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, db_index=True, blank=True)),
-            ('parent', self.gf('mptt.fields.TreeForeignKey')(blank=True, related_name='children', null=True, to=orm['portal.Channel'])),
+            ('parent', self.gf('mptt.fields.TreeForeignKey')(blank=True, related_name='children', null=True, to=orm['tinyportal.Channel'])),
             (u'lft', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
             (u'rght', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
             (u'tree_id', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
             (u'level', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
         ))
-        db.send_create_signal(u'portal', ['Channel'])
+        db.send_create_signal(u'tinyportal', ['Channel'])
 
         # Adding model 'Container'
-        db.create_table(u'portal_container', (
+        db.create_table(u'tinyportal_container', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('published', self.gf('django.db.models.fields.BooleanField')(default=False, db_index=True)),
             ('publish_date', self.gf('django.db.models.fields.DateTimeField')(db_index=True, null=True, blank=True)),
@@ -34,36 +34,36 @@ class Migration(SchemaMigration):
             ('description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
             ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, db_index=True, blank=True)),
             ('modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, db_index=True, blank=True)),
-            ('channel', self.gf('mptt.fields.TreeForeignKey')(to=orm['portal.Channel'])),
+            ('channel', self.gf('mptt.fields.TreeForeignKey')(to=orm['tinyportal.Channel'])),
         ))
-        db.send_create_signal(u'portal', ['Container'])
+        db.send_create_signal(u'tinyportal', ['Container'])
 
 
     def backwards(self, orm):
         # Deleting model 'Channel'
-        db.delete_table(u'portal_channel')
+        db.delete_table(u'tinyportal_channel')
 
         # Deleting model 'Container'
-        db.delete_table(u'portal_container')
+        db.delete_table(u'tinyportal_container')
 
 
     models = {
-        u'portal.channel': {
+        u'tinyportal.channel': {
             'Meta': {'object_name': 'Channel'},
             'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             u'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
             u'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
             'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True', 'blank': 'True'}),
-            'parent': ('mptt.fields.TreeForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': u"orm['portal.Channel']"}),
+            'parent': ('mptt.fields.TreeForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': u"orm['tinyportal.Channel']"}),
             u'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
             'slug': ('django_extensions.db.fields.AutoSlugField', [], {'allow_duplicates': 'False', 'max_length': '255', 'separator': "u'-'", 'blank': 'True', 'populate_from': "'title'", 'overwrite': 'True'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             u'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
         },
-        u'portal.container': {
+        u'tinyportal.container': {
             'Meta': {'object_name': 'Container'},
-            'channel': ('mptt.fields.TreeForeignKey', [], {'to': u"orm['portal.Channel']"}),
+            'channel': ('mptt.fields.TreeForeignKey', [], {'to': u"orm['tinyportal.Channel']"}),
             'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True', 'blank': 'True'}),
             'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'expire_date': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'}),
@@ -76,4 +76,4 @@ class Migration(SchemaMigration):
         }
     }
 
-    complete_apps = ['portal']
+    complete_apps = ['tinyportal']
