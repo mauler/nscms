f2ed907c92daace44fa3bde5771fe328b569e8bc
Paulo R <proberto.macedo@gmail.com>
2015-01-17 17:16:58 -0200

    Adds support for django-mptt-tree-editor

--- nscms/contrib/tinyportal/admin.py
+++ nscms/contrib/tinyportal/admin.py
@@ -5,9 +5,15 @@ from .models import Channel, Container
 
 
 def get_model_admin_class():
+
+    if 'mptt_tree_editor' in settings.INSTALLED_APPS:
+        from mptt_tree_editor.admin import TreeEditor
+        return TreeEditor
+
     if 'django_mptt_admin' in settings.INSTALLED_APPS:
         from django_mptt_admin.admin import DjangoMpttAdmin
         return DjangoMpttAdmin
+
     return ModelAdmin
 
 
