7bc8ae4457c4ecad1b82f3b155a7ab63a87c3bcf
Paulo R <proberto.macedo@gmail.com>
2015-01-17 18:26:04 -0200

    Adds support for feincms

--- nscms/contrib/tinyportal/admin.py
+++ nscms/contrib/tinyportal/admin.py
@@ -1,10 +1,15 @@
 from django.conf import settings
-from django.contrib.admin import site, ModelAdmin
+from django.contrib.admin import site
 
+from mptt.admin import MPTTModelAdmin
 from .models import Channel, Container
 
 
 def get_model_admin_class():
+
+    if 'feincms' in settings.INSTALLED_APPS:
+        from mptt.admin import FeinCMSModelAdmin
+        return FeinCMSModelAdmin
 
     if 'mptt_tree_editor' in settings.INSTALLED_APPS:
         from mptt_tree_editor.admin import TreeEditor
@@ -14,7 +19,7 @@ def get_model_admin_class():
         from django_mptt_admin.admin import DjangoMpttAdmin
         return DjangoMpttAdmin
 
-    return ModelAdmin
+    return MPTTModelAdmin
 
 
 class ChannelAdmin(get_model_admin_class()):
