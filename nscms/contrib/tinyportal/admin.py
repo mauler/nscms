2789d474416341977646428aa8790c863dffbd39
Paulo R <proberto.macedo@gmail.com>
2015-01-17 17:00:20 -0200

    Adds django-mptt-admin support

--- nscms/contrib/tinyportal/admin.py
+++ nscms/contrib/tinyportal/admin.py
@@ -1,7 +1,21 @@
-from django.contrib.admin import site
+from django.conf import settings
+from django.contrib.admin import site, ModelAdmin
 
 from .models import Channel, Container
 
 
-site.register(Channel)
+def get_model_admin_class():
+    if 'django_mptt_admin' in settings.INSTALLED_APPS:
+        from django_mptt_admin.admin import DjangoMpttAdmin
+        return DjangoMpttAdmin
+    return ModelAdmin
+
+
+class ChannelAdmin(get_model_admin_class()):
+    pass
+
+
+site.register(Channel, ChannelAdmin)
+
+
 site.register(Container)
